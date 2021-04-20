ccf055e8eadbe95a17d766aed447d34881e714de
Gspeak1109 <gspeak1109@gmail.com>
2021-04-20 13:02:30 +0530

    Create Package.py

---
+++ Package.py
@@ -0,0 +1,13 @@
+#Package
+#Package has large number of files needed for your code
+#common package every pythoneer should know i believe
+#NumPy
+#Pendulum
+#MoviePy
+#Request
+#Tkinters
+#PyOt
+#Pandas
+#Py32
+#pyTest
+#try study above
